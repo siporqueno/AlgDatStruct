d870ecdcccde4d715e921c56b64d21c75741fdf8
siporqueno <strike1000001@gmail.com>
2019-05-22 00:43:50 +0300

    Bubble sort

--- src/Preparation/TimeMeasurement.java
+++ src/Preparation/TimeMeasurement.java
@@ -1,8 +1,12 @@
 package Preparation;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class TimeMeasurement {
+
+    static TimeMeasurement timeMeasurement;
+    static int[] testArr = {10, 8, 6, 4, 2};
 
     static Random random = new Random();
 
@@ -17,9 +21,28 @@ public class TimeMeasurement {
         return finish - start;
     }
 
+    int[] bubbleSort(int[] inputArr) {
+        int temporaryVariable;
+
+        for (int i = inputArr.length - 1; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                if (inputArr[j - 1] > inputArr[j]) {
+                    temporaryVariable = inputArr[j - 1];
+                    inputArr[j - 1] = inputArr[j];
+                    inputArr[j] = temporaryVariable;
+                }
+            }
+        }
+
+        return inputArr;
+    }
+
     public static void main(String[] args) {
+        timeMeasurement = new TimeMeasurement();
+        System.out.println(Arrays.toString(testArr));
+        System.out.println(Arrays.toString(timeMeasurement.bubbleSort(testArr)));
         for (int i = 0; i < 100; i++) {
-            System.out.println((i +1)+" "+measure()/1000000);
+            System.out.println((i + 1) + " " + measure() / 1000000);
         }
     }
 }
