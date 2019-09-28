dc52c6c8ce22a81e25f31e7e34f56a0f14774b6c
siporqueno <strike1000001@gmail.com>
2019-09-28 13:03:46 +0300

    A bit of cats comparison through overriden method equals.

---
+++ src/Lesson_1/Cat.java
@@ -0,0 +1,30 @@
+package Lesson_1;
+
+public class Cat {
+    private String name;
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ( obj == this ) return true;
+        if (!( obj instanceof Cat)){
+            return false;
+        }
+        Cat c = ( Cat ) obj ;
+        return c.name == this.name ;
+    }
+}
---
+++ src/Lesson_1/MainL1.java
@@ -0,0 +1,11 @@
+package Lesson_1;
+
+public class MainL1 {
+    public static void main(String[] args) {
+        Cat cOne = new Cat("Мурзик");
+        Cat cTwo = new Cat("Мурзик");
+        Cat cThree = new Cat("Васька");
+        System.out.println(cOne.equals(cTwo) ? "Все одноименные коты равны" : "Нет, имя не делает котов равными");
+        System.out.println(cOne.equals(cThree) ? "Имя не делает разницы" : "Разница в имени тебя выделяет и отличает!");
+    }
+}
