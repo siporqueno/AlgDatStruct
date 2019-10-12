05e92554fd5da471ff0fa79256052957bfe9f423
siporqueno <strike1000001@gmail.com>
2019-10-12 12:49:18 +0300

    Home work of Lesson 4, realized Stack on base of single LinkedList.

---
+++ src/lessonFour/stack_from_list/Stack.java
@@ -0,0 +1,16 @@
+package lessonFour.stack_from_list;
+
+// Home work of Lesson 4
+public interface Stack {
+
+    public boolean isEmpty();
+
+    public void push(String name, int age);
+
+    public String pop();
+
+    public String peek();
+
+    public void display();
+
+}
