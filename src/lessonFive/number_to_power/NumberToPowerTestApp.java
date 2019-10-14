9f6a8b2a7e449fa7699d351b27b761e8f06d9a2e
siporqueno <strike1000001@gmail.com>
2019-10-14 23:02:56 +0300

    Home work of Lesson 5, task 1. Number to power.

---
+++ src/lessonFive/number_to_power/NumberToPowerTestApp.java
@@ -0,0 +1,11 @@
+package lessonFive.number_to_power;
+
+public class NumberToPowerTestApp {
+    public static void main(String[] args) {
+        NumberToPower numberToPower = new NumberToPower();
+        System.out.println(numberToPower.power(2,1));
+        System.out.println(numberToPower.power(2,2));
+        System.out.println(numberToPower.power(2,3));
+        System.out.println(numberToPower.power(3,18));
+    }
+}
